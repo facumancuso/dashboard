381bd915aa62d85b97163943dc6c858b3acbbe27
Facundo <facumancuso@hotmail.com>
2025-06-13 13:08:43 -0300

    button resolve

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -7,8 +7,6 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export function Button({ children, className, ...rest }: ButtonProps) {
   return (
     <button
-    <h1 className="text-blue-500">I'm blue!</h1>
-
       {...rest}
       className={clsx(
         'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
@@ -17,6 +15,5 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     >
       {children}
     </button>
-    
   );
 }
